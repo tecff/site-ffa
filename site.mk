68317a44b67f1d15ce9cd9aff82384b64768b707
Andreas Ziegler <github@andreas-ziegler.de>
2016-09-05 19:37:24 +0200

    site.mk: add GLUON_REGION to satisfy upstream gluon commit 639c12b

--- site.mk
+++ site.mk
@@ -201,7 +201,11 @@ DEFAULT_GLUON_RELEASE := snapshot~$(shell date '+%Y%m%d')
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
+# Default priority for updates.
 GLUON_PRIORITY ?= 0
+
+# Region code required for some images; supported values: us eu
+GLUON_REGION ?= eu
 
 # Languages to include
 GLUON_LANGS ?= en de
